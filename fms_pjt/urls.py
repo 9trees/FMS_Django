e6f6f2fa7368304e4ba8d8955e8cbccfb920bc24
9trees <sivashankaryadav@outlook.com>
2020-09-10 18:48:19 +0100

    progress bar added

--- fms_pjt/urls.py
+++ fms_pjt/urls.py
@@ -1,4 +1,4 @@
-"""FMS_PJT URL Configuration
+"""fms_pjt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- fms_pjt/wsgi.py
+++ fms_pjt/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FMS_PJT project.
+WSGI config for fms_pjt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FMS_PJT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fms_pjt.settings')
 
 application = get_wsgi_application()
